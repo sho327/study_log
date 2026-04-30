70296e998c95425441ebb62fa5be35418990fbe8
sho327 <49551360+sho327@users.noreply.github.com>
2026-04-30 23:24:44 +0900

    master_log_category_base.py を作成

---
+++ src/apps/log/serializers/master_log_category_base.py
@@ -0,0 +1,31 @@
+from rest_framework import serializers
+
+# --- ログモジュール ---
+from apps.log.models import M_LogCategory
+
+class MasterLogCategoryBaseSerializer(serializers.ModelSerializer):
+    """
+    ベース定義:モデルとの紐付けだけを行う。
+    直接は使わず、継承して利用する。
+    """
+    class Meta:
+        model = M_LogCategory
+        fields = "__all__" # 基本は全フィールド対象
+
+class MasterLogCategoryMiniResponseSerializer(MasterLogCategoryBaseSerializer):
+    """
+    【最小構成】一覧用
+    Metaを上書きして、IDと名称だけに絞り込む
+    """
+    class Meta(MasterLogCategoryBaseSerializer.Meta):
+        fields = [
+            "id",  
+            "name",
+        ]
+
+class MasterLogCategoryFullResponseSerializer(MasterLogCategoryBaseSerializer):
+    """
+    【最大構成】詳細用
+    Baseの定義(__all__)をそのまま使い、モデルの変更に自動追従させる
+    """
+    pass
