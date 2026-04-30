5ce7d90b8e87da2ca4cfb103fd173c3a0a7904bd
sho327 <49551360+sho327@users.noreply.github.com>
2026-04-30 23:19:00 +0900

    exceptions.py を作成

---
+++ src/apps/log/exceptions.py
@@ -0,0 +1,35 @@
+from rest_framework import status
+
+# --- コアモジュール ---
+from core.exceptions.exceptions import ApplicationError
+
+class LogError(ApplicationError):
+    """
+    ログ(Log)ドメインにおける全てのビジネス例外の基底クラス。
+    """
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    message_id = "ERR_LOG_000"
+    message = "Log Error"
+    detail = "ログ関連の処理中にエラーが発生しました。"
+
+# --------------------------------------------------
+# 登録・重複系 (409)
+# --------------------------------------------------
+
+# --------------------------------------------------
+# 参照・整合性系 (404)
+# --------------------------------------------------
+
+class LogNotFoundError(LogError):
+    """指定されたログトランが見つからない場合に発生"""
+    status_code = status.HTTP_404_NOT_FOUND
+    message_id = "ERR_LOG_001"
+    message = "Log Not Found Error"
+    detail = "指定されたログ情報が見つかりません。"
+
+class LogCommentNotFoundError(LogError):
+    """指定されたログコメントトランが見つからない場合に発生"""
+    status_code = status.HTTP_404_NOT_FOUND
+    message_id = "ERR_LOG_002"
+    message = "Log Comment Not Found Error"
+    detail = "指定されたロクコメンド情報が見つかりません。"
